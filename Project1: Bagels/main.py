25260dc316dddde2c7e370f120255a4dc3779157
Vamsi <vamsi.dev08@gmail.com>
2025-07-20 12:12:08 +0530

    format code

--- Project1: Bagels/main.py
+++ Project1: Bagels/main.py
@@ -31,10 +31,12 @@ import random
 
 def generate_number(MAX_DIGITS):
     """generates a random MAX_DIGITS long number using random module"""
-    return  str(random.randint(10 ** (MAX_DIGITS-1), 10 ** MAX_DIGITS - 1))
+    return str(random.randint(10 ** (MAX_DIGITS-1), 10 ** MAX_DIGITS - 1))
+
 
 def compare(user_number, computer_number):
-    """Compares the number with computer generated and return a string and boolean, if won then true else false"""
+    """Compares the number with computer generated and
+        return a string and boolean, if won then true else false"""
     if user_number == computer_number:
         return "You have got it correct!", True
     else:
@@ -50,7 +52,8 @@ def compare(user_number, computer_number):
         random.shuffle(output)
         return " ".join(output), False
 
-def game_start(MAXIMUM_CHANCES,MAX_DIGITS):
+
+def game_start(MAXIMUM_CHANCES, MAX_DIGITS):
     """this has the core logic of the game - includes game loop and life line loop"""
     print("Welcome to the Bagels Game!")
     print("Try to guess the number. Clues:")
@@ -58,35 +61,38 @@ def game_start(MAXIMUM_CHANCES,MAX_DIGITS):
     print("- Pico: correct digit in wrong position")
     print("- Bagels: no correct digits\n")
     running = True
-    while running: #GAME-LOOP
-       chances = MAXIMUM_CHANCES
-       print(f"Computer is thinking of a number of {MAX_DIGITS} digits long.")
-       computer_number = generate_number(MAX_DIGITS)
-       is_won = False
-       while chances > 0: #LIFE_LINE LOOP
-           user_number = input("Please enter your number: ")
-           output, is_won = compare(user_number,computer_number)
-           print(output)
-           if is_won:
-               break
-           else:
-               chances -= 1
-               print(f"You have {chances} more chances left, Try again.")
+    while running:  # GAME-LOOP
+        chances = MAXIMUM_CHANCES
+        print(f"Computer is thinking of a number of {MAX_DIGITS} digits long.")
+        computer_number = generate_number(MAX_DIGITS)
+        is_won = False
+        while chances > 0:  # LIFE_LINE LOOP
+            user_number = input("Please enter your number: ")
+            output, is_won = compare(user_number, computer_number)
+            print(output)
+            if is_won:
+                break
+            else:
+                chances -= 1
+                print(f"You have {chances} more chances left, Try again.")
 
-       if not is_won:
-           print("Sorry, you lost!")
-       print(f"The computer number was {computer_number}")
-       play_again = input("Do you want to play again? (y/n): ")
-       if play_again == "n":
-           print("Thank you for playing!")
-           running = False
-       else:
-           continue
+        if not is_won:
+            print("Sorry, you lost!")
+        print(f"The computer number was {computer_number}")
+        play_again = input("Do you want to play again? (y/n): ")
+        if play_again == "n":
+            print("Thank you for playing!")
+            running = False
+        else:
+            continue
+
 
 def main():
     MAX_DIGITS = 3
     MAXIMUM_CHANCES = 10
-    game_start(MAXIMUM_CHANCES,MAX_DIGITS)
+    game_start(MAXIMUM_CHANCES, MAX_DIGITS)
+
 
 if __name__ == "__main__":
     main()
+
